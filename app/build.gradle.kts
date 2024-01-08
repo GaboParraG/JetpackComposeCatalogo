25b8f6651ccc5294c82963722ebf66fc68a15037
gabriel david parra <58826900+GaboParraG@users.noreply.github.com>
2024-01-08 12:09:09 -0500

    Se crea una vista aplicando Columns, Rows y constraint

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.jetpackcomposecatalogo"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jetpackcomposecatalogo"
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.jetpackcomposecatalogo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.jetpackcomposecatalogo.ui.theme.JetpackComposeCatalogoTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyExample()
                 }
             }
         }
@@ -30,17 +41,92 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyExample() {
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(color = Color.Gray)){
+        Column(modifier= Modifier.fillMaxSize()) {
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(color = Color.Blue))
+            Spacer(modifier = Modifier
+                .width(0.dp)
+                .height(5.dp)
+                .background(color = Color.White))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(color = Color.Red)){
+                ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+                    val (boxWhite, boxGray, boxMagenta, boxBlack, boxYellow) = createRefs()
+
+                    Box(modifier = Modifier
+                        .size(55.dp)
+                        .background(Color.White)
+                        .constrainAs(boxWhite) {
+                            top.linkTo(parent.top)
+                            start.linkTo(parent.start)
+                            end.linkTo(parent.end)
+                            bottom.linkTo(parent.bottom)
+                        })
+                    Box(modifier = Modifier
+                        .size(55.dp)
+                        .background(Color.Gray)
+                        .constrainAs(boxGray) {
+                            bottom.linkTo(boxWhite.top)
+                            end.linkTo(boxWhite.start)
+                        })
+                    Box(modifier = Modifier
+                        .size(55.dp)
+                        .background(Color.Magenta)
+                        .constrainAs(boxMagenta) {
+                            bottom.linkTo(boxWhite.top)
+                            start.linkTo(boxWhite.end)
+                        })
+                    Box(modifier = Modifier
+                        .size(55.dp)
+                        .background(Color.Black)
+                        .constrainAs(boxBlack) {
+                            top.linkTo(boxWhite.bottom)
+                            end.linkTo(boxWhite.start)
+                        })
+                    Box(modifier = Modifier
+                        .size(55.dp)
+                        .background(Color.Yellow)
+                        .constrainAs(boxYellow) {
+                            top.linkTo(boxWhite.bottom)
+                            start.linkTo(boxWhite.end)
+                        })
+                }
+            }
+            Spacer(modifier = Modifier
+                .width(0.dp)
+                .height(5.dp)
+                .background(color = Color.White))
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .weight(1f)
+                .background(color = Color.Magenta)){
+                Row {
+                    Box(modifier = Modifier.size(125.dp).background(Color.Yellow))
+                    Spacer(modifier = Modifier
+                        .width(5.dp)
+                        .height(0.dp)
+                        .background(color = Color.Blue))
+                    Box(modifier = Modifier.size(125.dp).background(Color.Red))
+                }
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetpackComposeCatalogoTheme {
-        Greeting("Android")
+        MyExample()
     }
 }
